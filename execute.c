69719c943b3acd486d1554d89bacd054e9ffdbf0
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2026-04-13 09:22:16 +0200

    Implement command execution with fork and execvp

--- execute.c
+++ execute.c
@@ -1,3 +1,21 @@
+#include <stdio.h>
+#include <unistd.h> 
+#include <sys/wait.h>
+
 void execute_command(char** argv) {
-    
+    if (argv[0] == NULL) {
+        return;
+    }   
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        execvp(argv[0], argv);
+        fprintf(stderr, "hsh: command not found: %s\n", argv[0]);
+        _exit(1);
+    } else if (pid > 0) {
+        waitpid(pid, NULL, 0);
+    } else {
+        perror("fork");
+    }
 }
