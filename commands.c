af8374508cb24af1adfd5ddb69ddd7191c9d8689
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2026-05-02 19:44:26 +0200

    Add stubs for built-in command functions

---
+++ commands.c
@@ -0,0 +1,19 @@
+#include "hsh.h"
+
+void hsh_echo(char** argv) {
+}
+
+void hsh_cat(char** argv) {
+}
+
+void hsh_touch(char** argv) {
+}
+
+void hsh_cd(char** argv) {
+}
+
+void hsh_pwd(char** argv) {
+}
+
+void hsh_exit(char** argv) {
+}
--- hsh.h
+++ hsh.h
@@ -6,4 +6,11 @@ void read_input(char* input);
 void parse_input(char* input, char** argv);
 void execute_command(char** argv);
 
+void hsh_echo(char** argv);
+void hsh_cat(char** argv);
+void hsh_touch(char** argv);
+void hsh_cd(char** argv);
+void hsh_pwd(char** argv);
+void hsh_exit(char** argv);
+
 #endif
