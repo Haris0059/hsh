2233e38d720b6557b6f1e32af0a0db5c9a46b792
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2026-05-03 01:52:07 +0200

    Implement echo, cat, touch and add output redirection

--- commands.c
+++ commands.c
@@ -1,15 +1,69 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include "hsh.h"
 
 void hsh_echo(char** argv) {
+    int i = 1;
+    int newline = 1;
+    int escapes = 0;
+
+    if (argv[i] && strcmp(argv[i], "-n") == 0) { newline = 0; i++; }
+    if (argv[i] && strcmp(argv[i], "-e") == 0) { escapes = 1; i++; }
+
+    for (; argv[i]; i++) {
+        if (escapes) {
+            for (char* p = argv[i]; *p; p++) {
+                if (*p == '\\' && *(p+1) == 'n') { putchar('\n'); p++; }
+                else if (*p == '\\' && *(p+1) == 't') { putchar('\t'); p++; }
+                else putchar(*p);
+            }
+        } else printf("%s", argv[i]);
+        if (argv[i+1]) putchar(' ');
+    }
+    if (newline) putchar('\n');
 }
 
 void hsh_cat(char** argv) {
+    char buf[1024];
+    ssize_t n;
+
+    if (argv[1] == NULL) {
+        while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0)
+            write(STDOUT_FILENO, buf, n);
+        return;
+    }
+
+    for (int i = 1; argv[i]; i++) {
+        int fd = open(argv[i], O_RDONLY);
+        if (fd < 0) { perror(argv[i]); continue; }
+        while ((n = read(fd, buf, sizeof(buf))) > 0)
+            write(STDOUT_FILENO, buf, n);
+        close(fd);
+    }
 }
 
 void hsh_touch(char** argv) {
+    if (argv[1] == NULL) {
+        fprintf(stderr, "touch: missing file operand\n");
+        return;
+    }
+
+    for (int i = 1; argv[i] != NULL; i++) {
+        int fd = open(argv[i], O_WRONLY | O_CREAT, 0644);
+        if (fd < 0) {
+            perror(argv[i]);
+            continue;
+        }
+        close(fd);
+
+        if (utimensat(AT_FDCWD, argv[i], NULL, 0) != 0) {
+            perror(argv[i]);
+        }
+    }
 }
 
 void hsh_cd(char** argv) {
--- execute.c
+++ execute.c
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -7,24 +8,50 @@
 void execute_command(char** argv) {
     if (argv[0] == NULL) {
         return;
-    }   
+    }
 
-    if (strcmp(argv[0], "exit") == 0)  { hsh_exit(argv);  return; }
-    if (strcmp(argv[0], "pwd") == 0)   { hsh_pwd(argv);   return; }
-    if (strcmp(argv[0], "cd") == 0)    { hsh_cd(argv);    return; }
-    if (strcmp(argv[0], "echo") == 0)  { hsh_echo(argv);  return; }
-    if (strcmp(argv[0], "cat") == 0)   { hsh_cat(argv);   return; }
-    if (strcmp(argv[0], "touch") == 0) { hsh_touch(argv); return; }
+    // scan for > and redirect stdout to the given file
+    char* outfile = NULL;
+    for (int i = 0; argv[i]; i++) {
+        if (strcmp(argv[i], ">") == 0) {
+            outfile = argv[i+1];
+            argv[i] = NULL;
+            break;
+        }
+    }
 
-    pid_t pid = fork();
+    int saved = -1;
+    if (outfile) {
+        int fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd < 0) { perror(outfile); return; }
+        saved = dup(STDOUT_FILENO);
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
+    }
 
-    if (pid == 0) {
-        execvp(argv[0], argv);
-        fprintf(stderr, "hsh: command not found: %s\n", argv[0]);
-        _exit(1);
-    } else if (pid > 0) {
-        waitpid(pid, NULL, 0);
-    } else {
-        perror("fork");
+    if      (strcmp(argv[0], "exit") == 0)  hsh_exit(argv);
+    else if (strcmp(argv[0], "pwd") == 0)   hsh_pwd(argv);
+    else if (strcmp(argv[0], "cd") == 0)    hsh_cd(argv);
+    else if (strcmp(argv[0], "echo") == 0)  hsh_echo(argv);
+    else if (strcmp(argv[0], "cat") == 0)   hsh_cat(argv);
+    else if (strcmp(argv[0], "touch") == 0) hsh_touch(argv);
+    else {
+        pid_t pid = fork();
+
+        if (pid == 0) {
+            execvp(argv[0], argv);
+            fprintf(stderr, "hsh: command not found: %s\n", argv[0]);
+            _exit(1);
+        } else if (pid > 0) {
+            waitpid(pid, NULL, 0);
+        } else {
+            perror("fork");
+        }
+    }
+
+    fflush(stdout);
+    if (saved != -1) {
+        dup2(saved, STDOUT_FILENO);
+        close(saved);
     }
 }
